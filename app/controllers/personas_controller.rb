442c4615937d652cd936a730afdb8d6884b31d59
Helton Bustos <heltonsmith@hotmail.com>
2022-05-04 12:32:54 -0400

    mostre los correos

--- app/controllers/personas_controller.rb
+++ app/controllers/personas_controller.rb
@@ -2,6 +2,9 @@ class PersonasController < ApplicationController
 
     def index 
         @resultado = Persona.all
+        @correos = Persona.find(1).correos
     end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  #resources :correos
-  resources :personas
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   #get "/", to: "persona#ver"
+
+  resources :correos
+  resources :personas
+
+
 end
